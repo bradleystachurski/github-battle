311fdaacd9e998aa86f77c77df2eaa0e05fcc1b7
Bradley Stachurski <bradley.stachurski@gmail.com>
2017-04-21 12:06:38 -0600

    Started navigation component

--- app/index.js
+++ app/index.js
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import './index.css'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
-  }
-}
+import App from './components/App'
 
 ReactDOM.render(
   <App/>,
